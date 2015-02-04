9d1769037ab19481ea665147e29f693f9b642193
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-02-04 05:27:12 +0000

    Fix chefspec testcases.

--- site-cookbooks/fluentd_part/spec/recipes/all_configure_spec.rb
+++ site-cookbooks/fluentd_part/spec/recipes/all_configure_spec.rb
@@ -3,6 +3,20 @@ require_relative '../spec_helper'
 describe 'fluentd_part::all_configure' do
   let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
+  it 'create pos_dir' do
+    allow(File).to receive(:exist?).and_return(false)
+    expect(chef_run).to create_directory('/var/log/td-agent/pos').with(
+      owner: 'td-agent',
+      group: 'td-agent',
+      mode: 0755,
+      recursive: true
+    )
+  end
+
+  it 'include common recipe' do
+    expect(chef_run).to include_recipe 'fluentd_part::common'
+  end
+
   it 'install client config file' do
     expect(chef_run).to create_template('/etc/td-agent/config.d/client.conf').with(
       owner: 'td-agent',
