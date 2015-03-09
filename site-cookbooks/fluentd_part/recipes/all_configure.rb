1001f47e18b4cc2626f923a4ef1f309b3e449bd1
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-03-09 02:19:08 +0000

    Add log config generation at deploy.

--- site-cookbooks/fluentd_part/recipes/all_configure.rb
+++ site-cookbooks/fluentd_part/recipes/all_configure.rb
@@ -11,13 +11,28 @@ directory node['fluentd_part']['client']['pos_dir'] do
 end
 
 include_recipe 'fluentd_part::common'
-
-log_collection_config = {}
-Dir.glob(File.join(node['fluentd_part']['client']['patterns_dir'], '*')).each do |pattern_dir|
+patterns_dir = File.join(node['fluentd_part']['client']['patterns_dir'], '*')
+log_collection_config = Dir.glob(patterns_dir).inject({}) do |result, pattern_dir|
   log_colleciton_file = File.join(pattern_dir, node['fluentd_part']['client']['log_config_file'])
-  next unless File.exist?(log_colleciton_file)
-  ::Chef::Mixin::DeepMerge.deep_merge!(YAML.load_file(log_colleciton_file), log_collection_config)
+  next result unless File.exist?(log_colleciton_file)
+  ::Chef::Mixin::DeepMerge.deep_merge!(YAML.load_file(log_colleciton_file), result)
 end
+
+parameters = CloudConductorUtils::Consul.read_parameters[:cloudconductor]
+applications = parameters[:applications] || {}
+deploy_log_collection_config = applications.inject({}) do |result, (application_name, application)|
+  next result if application[:parameters].nil? || application[:parameters][:log_collection].nil?
+  application_config = application[:parameters][:log_collection].inject({}) do |role_config, (role, paths)|
+    ::Chef::Mixin::DeepMerge.deep_merge!(
+      {
+        role.to_s => paths
+      },
+      role_config
+    )
+  end
+  ::Chef::Mixin::DeepMerge.deep_merge!(application_config, result)
+end
+::Chef::Mixin::DeepMerge.deep_merge!(deploy_log_collection_config, log_collection_config)
 ::Chef::Mixin::DeepMerge.deep_merge!(node['fluentd_part']['client']['target'], log_collection_config)
 roles = ENV['ROLE'].split(',').unshift('all')
 log_collection_config.select! do |key|
