d55b815e73a186610023ceed2d42423a0d18c4c7
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-03-17 01:00:34 +0000

    Fix serverspec files.

--- serverspec/spec/log/log_configure_spec.rb
+++ serverspec/spec/log/log_configure_spec.rb
@@ -7,16 +7,3 @@ end
 describe port(24_224) do
   it { should be_listening.with('tcp') }
 end
-
-describe 'connect td-agent' do
-  servers = property[:servers]
-
-  servers.each do |svr_name, server|
-    next unless server[:roles] == 'log'
-    describe "#{svr_name} access check" do
-      describe command("hping3 -S #{server[:private_ip]} -p 24224 -c 5") do
-        its(:stdout) { should match '/sport=24224 flags=SA/' }
-      end
-    end
-  end
-end
