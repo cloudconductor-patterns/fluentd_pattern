65e395535f46c857dc0a178c900905974ada2c75
b03b88c9a1df <b03b88c9a1df@gmail.com>
2015-04-14 00:37:43 +0000

    Fix td-agent version.

--- site-cookbooks/fluentd_part/attributes/default.rb
+++ site-cookbooks/fluentd_part/attributes/default.rb
@@ -2,6 +2,8 @@ include_attribute 'td-agent'
 default['td_agent']['plugins'] = [
   'forest'
 ]
+default['td_agent']['pinning_version'] = true
+default['td_agent']['version'] = '2.1.5-1'
 default['fluentd_part']['server']['store_dir'] = '/var/log/td-agent/in_forward'
 default['fluentd_part']['client']['patterns_dir'] = '/opt/cloudconductor/patterns'
 default['fluentd_part']['client']['log_config_file'] = 'config/log_collection.yml'
